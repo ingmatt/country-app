8ec7dc56b123a246076c9febad30d4ecb161a03b
Matthew Ing <108230760+ingmatt@users.noreply.github.com>
2023-10-07 11:23:06 +0200

    Add files via upload

--- index.jsx
+++ index.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Layout from "./pages/Layout"
+import Home from "./pages/Home"
+import CountryDetail from "./pages/CountryDetail"
+import RegionDetail from "./pages/RegionDetail";
+import RegionSelect from "./components/RegionSelect"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={<RegionSelect/>} />
+          <Route path="/country/:country" element={<CountryDetail />} />
+          <Route path="/region/:region" element={<RegionDetail />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />)
